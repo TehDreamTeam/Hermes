8c23b0cd0bc66a9f5173e3eec91a3b7e8caa6e98
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2018-02-02 17:31:42 +0100

    Added Service class.

---
+++ core/src/main/java/nl/tehdreamteam/hermes/core/Service.java
@@ -0,0 +1,10 @@
+package nl.tehdreamteam.hermes.core;
+
+public interface Service {
+
+    String getHostname();
+
+    int getPort();
+
+    String getFullAddress();
+}
---
+++ core/src/main/java/nl/tehdreamteam/hermes/core/service/HermesService.java
@@ -0,0 +1,37 @@
+package nl.tehdreamteam.hermes.core.service;
+
+import nl.tehdreamteam.hermes.core.Service;
+
+public class HermesService implements Service {
+
+    private String hostname;
+    private int port;
+
+    public HermesService(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    @Override
+    public String getHostname() {
+        return this.hostname;
+    }
+
+    @Override
+    public int getPort() {
+        return this.port;
+    }
+
+    @Override
+    public String getFullAddress() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("http://");
+        builder.append(hostname);
+        builder.append(':');
+        builder.append(port);
+        builder.append("/");
+
+        return builder.toString();
+    }
+}
