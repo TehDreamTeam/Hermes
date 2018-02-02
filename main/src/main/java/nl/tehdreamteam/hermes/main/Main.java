83a8aa52905b4336cfac7bad3a24657eb1f9c796
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2018-02-02 17:32:43 +0100

    Added temp code to main.

--- main/src/main/java/nl/tehdreamteam/hermes/main/Main.java
+++ main/src/main/java/nl/tehdreamteam/hermes/main/Main.java
@@ -1,22 +1,22 @@
 package nl.tehdreamteam.hermes.main;
 
 import nl.tehdreamteam.hermes.core.HermesServer;
+import nl.tehdreamteam.hermes.core.Service;
+import nl.tehdreamteam.hermes.core.ServiceLocator;
 import nl.tehdreamteam.hermes.core.grizzly.GrizzlyHermesServer;
+import nl.tehdreamteam.hermes.core.handler.LocateServiceHandler;
+import nl.tehdreamteam.hermes.core.service.HermesService;
 import org.glassfish.grizzly.http.server.HttpHandler;
-import org.glassfish.grizzly.http.server.Request;
-import org.glassfish.grizzly.http.server.Response;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.Optional;
 
 public class Main {
 
     public static void main(String... args) throws IOException {
         HermesServer server = new GrizzlyHermesServer(1201);
 
-        server.addHandler(new HttpHandler() {
+        /*server.addHandler(new HttpHandler() {
             @Override
             public void service(Request request, Response response) throws Exception {
                 final SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
@@ -25,8 +25,21 @@ public class Main {
                 response.setContentLength(date.length());
                 response.getWriter().write(date);
             }
-        });
+        });*/
 
+        ServiceLocator locator = new ServiceLocator() {
+            @Override
+            public Optional<Service> getService(String name) {
+                if (name.equals("Hans")) {
+                    return Optional.of(new HermesService("localhost", 1202));
+                }
+                return Optional.empty();
+            }
+        };
+
+        HttpHandler locateHandler = new LocateServiceHandler(locator);
+
+        server.addHandler(locateHandler);
 
         server.start();
 
