4a415f804f9f3c5c11d2ff5b268f73235e7e1c0b
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2018-02-05 09:12:00 +0100

    Crappy register code.

--- core/src/main/java/nl/tehdreamteam/hermes/server/core/handler/RegisterServiceHandler.java
+++ core/src/main/java/nl/tehdreamteam/hermes/server/core/handler/RegisterServiceHandler.java
@@ -1,14 +1,19 @@
 package nl.tehdreamteam.hermes.server.core.handler;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nl.tehdreamteam.hermes.server.core.Service;
 import nl.tehdreamteam.hermes.server.core.ServiceManager;
+import nl.tehdreamteam.hermes.server.core.service.HermesService;
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
-import org.glassfish.grizzly.http.util.HttpStatus;
+
+import java.io.IOException;
 
 public class RegisterServiceHandler extends HttpHandler {
 
-    ServiceManager manager;
+    private ServiceManager manager;
 
     public RegisterServiceHandler(ServiceManager manager) {
         this.manager = manager;
@@ -20,7 +25,23 @@ public class RegisterServiceHandler extends HttpHandler {
     }
 
     @Override
-    public void service(Request request, Response response) {
-        response.setStatus(HttpStatus.HTTP_VERSION_NOT_SUPPORTED_505);
+    public void service(Request request, Response response) throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = null;
+
+        try {
+            node = mapper.readTree(request.getInputStream());
+            System.out.println(node);
+        } catch (IOException e) {
+            response.sendError(1337, "Je moeder is een slechte JSON.");
+        }
+
+        String name = node.get("name").asText();
+        String hostname = node.get("hostname").asText();
+        int port = node.get("port").asInt();
+
+        Service service = new HermesService(hostname, port);
+        manager.registerService(name, service);
     }
 }
